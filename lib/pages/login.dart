257c6a1856e1e4104c4be40cec60e4637697c401
gabrielpires11 <gabri4590@gmail.com>
2024-12-03 15:07:28 -0300

    atualizações com login do google

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gabriel_str/pages/cadastro1.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:gabriel_str/pages/selecaoperfil.dart'; // Importa o pacote de autenticação do Firebase
-
+import 'package:google_sign_in/google_sign_in.dart'; // Pacote Google Sign-In
+import 'package:gabriel_str/pages/selecaoperfil.dart';
 class Login extends StatefulWidget {
   const Login({super.key});
 
@@ -11,20 +11,17 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final FirebaseAuth _auth = FirebaseAuth.instance; // Instância do FirebaseAuth para autenticação
-  final TextEditingController _emailController = TextEditingController(); // Controlador para o campo de e-mail
-  final TextEditingController _passwordController = TextEditingController(); // Controlador para o campo de senha
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn(); // Instância para autenticação do Google
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  // Método assíncrono para realizar o login
   Future<void> _login() async {
     try {
-      // Tenta fazer login com e-mail e senha fornecidos
       await _auth.signInWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
-
-      // Se o login for bem-sucedido, navega para a tela inicial
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
@@ -32,169 +29,236 @@ class _LoginState extends State<Login> {
         ),
       );
     } catch (e) {
-      // Em caso de erro, exibe uma notificação com a mensagem de erro
-      _showSnackBar('Email e/o senha inválidos', Colors.red);
+      _showSnackBar('Email e/ou senha inválidos', Colors.red);
     }
   }
 
-  // Método para exibir uma mensagem na parte inferior da tela (SnackBar)
+  Future<void> _loginWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        _showSnackBar('Login cancelado', Colors.orange);
+        return;
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      await _auth.signInWithCredential(credential);
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SelecaoPerfil(),
+        ),
+      );
+    } catch (e) {
+      _showSnackBar('Falha no login com Google $Error', Colors.red);
+    }
+  }
+
   void _showSnackBar(String message, Color color) {
     final snackBar = SnackBar(
       content: Text(message),
       backgroundColor: color,
-      duration: Duration(seconds: 2), // Duração de exibição do SnackBar
+      duration: Duration(seconds: 2),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
-  bool _showPassword = false;
 
+  bool _showPassword = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Colors.blue,
-      //   title: Text("Faça Login"),
-      // ),
       body: Stack(
         children: [
-          // Imagem de fundo
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage("assets/img/fundo.png"),
-                 // Substitua pelo caminho da sua imagem de fundo
-                fit: BoxFit.cover, // Ajusta a imagem para cobrir todo o espaço do background
+                fit: BoxFit.cover,
               ),
             ),
           ),
-          // Centralizando o conteúdo verticalmente
           Center(
             child: Column(
-              mainAxisSize: MainAxisSize.min, // Centraliza na vertical
+              mainAxisSize: MainAxisSize.min,
               children: [
-                // SizedBox(height: 100),
-                // Imagem do astronauta
                 Image.asset(
-                  'assets/img/astronalta.png', // Substitua pelo caminho da sua imagem
-                  height: 200, // Ajuste a altura conforme necessário
-                  width: 200, // Ajuste a largura conforme necessário
+                  'assets/img/astronalta.png',
+                  height: 200,
+                  width: 200,
                 ),
-                SizedBox(height: 10), // Espaço mínimo entre a imagem e o formulário
-                // Formulário
+                SizedBox(height: 10),
                 Container(
-                  width: MediaQuery.of(context).size.width * 0.8, // Diminuindo a largura do formulário
+                  width: MediaQuery.of(context).size.width * 0.8,
                   padding: const EdgeInsets.all(16.0),
                   decoration: BoxDecoration(
-                    color: Color(0xFF1E1E1E), // Cor de fundo do formulário #1E1E1E
+                    color: Color(0xFF1E1E1E),
                     borderRadius: BorderRadius.circular(8),
                   ),
-                  child: Form(
-                    // key: _formKey,
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          TextFormField(
-                            controller: _emailController,
-                            autofocus: true,
-                            decoration: InputDecoration(
-                              iconColor: Colors.black,
-                              icon: Icon(Icons.mail),
-                              hintText: "Informe o e-mail",
-                              hintStyle: TextStyle(color: Colors.white),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.mail),
+                          hintText: "Informe o e-mail",
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: !_showPassword,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.lock),
+                          suffixIcon: GestureDetector(
+                            child: Icon(
+                              _showPassword
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
                             ),
-                            style: TextStyle(color: Colors.white), // Adicione esta linha
-                            validator: (String? email) {
-                              if (email == "" || email == null) {
-                                return "O e-mail não pode ser vazio";
-                              }
-                              if (email.length < 6) {
-                                return "O e-mail está muito curto";
-                              }
-                              if (!email.contains("@")) {
-                                return "O e-mail é inválido";
-                              }
-                              return null;
+                            onTap: () {
+                              setState(() {
+                                _showPassword = !_showPassword;
+                              });
                             },
                           ),
-                          TextFormField(
-                            controller: _passwordController,
-                            autofocus: true,
-                            obscureText: !_showPassword,
-                            decoration: InputDecoration(
-                              iconColor: Colors.black,
-                              icon: const Icon(Icons.lock),
-                              suffixIcon: GestureDetector(
-                                child: Icon(
-                                  _showPassword
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                ),
-                                onTap: () {
-                                  setState(() {
+                          hintText: "Informe a senha",
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(height: 10),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: _login,
+                          child: Text("Entrar"),
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size(120, 50),
+                            backgroundColor: Colors.purple,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                       Center(
+                                child: TextButton(
+                                  onPressed: () async {
+                                    GoogleSignIn googleSignIn = GoogleSignIn();
 
-                                    _showPassword = !_showPassword;
-                                  });
-                                },
-                              ),
-                              hintText: "Informe a senha",
-                              hintStyle: TextStyle(color: Colors.white),
-                            ),
-                            style: TextStyle(color: Colors.white), // Adicione esta linha
-                            validator: (String? senha) {
-                              if (senha == "" || senha == null) {
-                                return "A senha não pode ser vazia";
-                              }
-                              return null;
-                            },
-                          ),
-                          SizedBox(height: 10),
-                            Center(
-                                child: ElevatedButton(
-                                  onPressed: _login,
-                                  child: Text("Entrar"),
-                                  style: ElevatedButton.styleFrom(
-                                    fixedSize: Size(120, 50),
-                                    backgroundColor: Colors.purple,
-                                    foregroundColor: Colors.white,
+                                    try {
+                                      GoogleSignInAccount? googleUser =
+                                          await googleSignIn.signIn();
+
+                                      if (googleUser == null) {
+                                        _showSnackBar(
+                                            'Login cancelado pelo usuário.',
+                                            Colors.red);
+                                        return;
+                                      }
+
+                                      GoogleSignInAuthentication googleAuth =
+                                          await googleUser.authentication;
+
+                                      OAuthCredential credential =
+                                          GoogleAuthProvider.credential(
+                                        accessToken: googleAuth.accessToken,
+                                        idToken: googleAuth.idToken,
+                                      );
+
+                                      UserCredential userCredential =
+                                          await FirebaseAuth.instance
+                                              .signInWithCredential(credential);
+
+                                      User? user = userCredential.user;
+                                      if (user != null) {
+                                        print(
+                                            'Usuário autenticado: ${user.displayName}');
+                                        _showSnackBar(
+                                            'Bem-vindo, ${user.displayName}!',
+                                            Colors.purple);
+
+                                        Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  SelecaoPerfil()),
+                                        );
+                                      }
+                                    } catch (e) {
+                                      _showSnackBar('Erro ao fazer login: $e',
+                                          Colors.red);
+                                    }
+                                  },
+                                  style: TextButton.styleFrom(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 5,
+                                        horizontal: 16), // Ajustando o padding
+                                    backgroundColor: Colors.white,
                                     shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                    fixedSize: Size(230,
+                                        50), // Ajustando o tamanho do botão
+                                  ),
+                                  child: Center(
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Image.asset('assets/icons/google.png',
+                                            height: 16), // Menor ícone
+                                        SizedBox(width: 8),
+                                        Text(
+                                          'Continuar com o Google',
+                                          style: TextStyle(
+                                            fontSize:
+                                                15, // Ajustando o tamanho da fonte
+                                            fontWeight: FontWeight.w600,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                        
+                                      ],
                                     ),
                                   ),
                                 ),
-                            ),
-
-                          SizedBox(height: 15),
-                          Divider(
-                            color: Colors.black,
-                          ),
-                          InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const Cadastro1(),
-                                ),
-                              );
-                            },
-                            child: GestureDetector(
-                              child: Text(
-                                "Cadastrar",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  decoration: TextDecoration.underline, // Adiciona sublinhado para parecer um link
-                                ),
                               ),
+                      SizedBox(height: 15),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Cadastro1(),
                             ),
+                          );
+                        },
+                        child: Text(
+                          "Cadastrar",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.white,
+                            decoration: TextDecoration.underline,
                           ),
-                          const Text("Se não tiver cadastro clique no link acima"),
-                        ],
+                        ),
                       ),
-                    ),
+                      const Text(
+                          "Se não tiver cadastro clique no link acima"),
+                    ],
                   ),
                 ),
               ],
@@ -205,4 +269,3 @@ class _LoginState extends State<Login> {
     );
   }
 }
-
